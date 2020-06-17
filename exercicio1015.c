4691eb5cd09cff76f609d4bfcd2303a0ac081147
toniz4 <cassioavila000@gmail.com>
2020-06-17 18:50:16 -0300

    adicionado exercicio1015

---
+++ exercicio1015.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double x1, x2, y1, y2, dist;
+
+    scanf("%lf %lf\n%lf %lf", &x1, &y1, &x2, &y2 );
+
+    dist = sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+    printf("%.4lf\n", dist);
+    return 0;
+}
